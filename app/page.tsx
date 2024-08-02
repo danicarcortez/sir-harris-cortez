20daa0a66fe7edba333314d1c0681322d737871d
danicarcortez <missdannyrose@gmail.com>
2024-08-02 11:16:34 -0500

    add icons for contact info

--- app/page.tsx
+++ app/page.tsx
@@ -51,6 +51,61 @@ const FigureComponent = ({
   </Popover>
 );
 
+const ContactCard = ({
+  name,
+  phoneNumber,
+}: {
+  name: string;
+  phoneNumber: string;
+}) => (
+  <Card className="max-w-sm">
+    <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+      {name}
+    </h5>
+
+    <section className="grid gap-1 grid-cols-[40px_40px]">
+      <a
+        className="font-normal text-gray-700 dark:text-gray-400"
+        href={`sms://${phoneNumber}`}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          className="size-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
+          />
+        </svg>
+      </a>
+      <a
+        className="font-normal text-gray-700 dark:text-gray-400"
+        href={`tel://${phoneNumber}`}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          className="size-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z"
+          />
+        </svg>
+      </a>
+    </section>
+  </Card>
+);
+
 export default function Home() {
   return (
     <Flowbite>
@@ -316,23 +371,9 @@ export default function Home() {
       <section>
         <SectionHeader text="Contact Information" id="contact-information" />
 
-        <div className="grid gap-2 grid-cols-2 grid-rows-1">
-          <Card className="max-w-sm">
-            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Juan Cortez
-            </h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">
-              915-227-6803
-            </p>
-          </Card>
-          <Card className="max-w-sm">
-            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Danica Cortez
-            </h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">
-              425-219-9285
-            </p>
-          </Card>
+        <div className="grid gap-2 grid-cols-2">
+          <ContactCard name="Juan Cortez" phoneNumber="9152276803" />
+          <ContactCard name="Danica Cortez" phoneNumber="4252199285" />
         </div>
       </section>
     </Flowbite>
