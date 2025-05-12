aabf42317c556211176b4759cc253948473eb179
Juan Cortez <juan_cortez@utexas.edu>
2025-05-12 17:57:17 -0500

    add new pictures

--- app/_components/Figure.tsx
+++ app/_components/Figure.tsx
@@ -1,31 +1,19 @@
-import { Popover } from "flowbite-react";
+import { Popover } from 'flowbite-react';
 
-export const FigureComponent = ({
-  imageSrc,
-  text,
-  variant = "sup",
-}: {
-  imageSrc: string;
-  text: string;
-  variant?: "sup" | "p";
-}) => (
+export const FigureComponent = ({ imageSrc, text, variant = 'sup' }: { imageSrc: string; text: string; variant?: 'sup' | 'p' }) => (
   <Popover
     trigger="hover"
     content={
-      <div className="max-w-48 md:max-w-72 max-h-48 md:max-h-72 text-sm text-gray-500 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400">
+      <div className="max-w-72 max-h-72 text-sm text-gray-500 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400">
         <img src={imageSrc} className="col-span-2 w-full" />
       </div>
     }
   >
     <>
-      {variant === "sup" ? (
-        <sup className="cursor-pointer inline text-blue-600 dark:text-blue-500">
-          {text}
-        </sup>
+      {variant === 'sup' ? (
+        <sup className="cursor-pointer inline text-blue-600 dark:text-blue-500">{text}</sup>
       ) : (
-        <p className="cursor-pointer inline text-blue-600 dark:text-blue-500">
-          {text}
-        </p>
+        <p className="cursor-pointer inline text-blue-600 dark:text-blue-500">{text}</p>
       )}
     </>
   </Popover>
--- app/_components/TimelineItem/DayOf.tsx
+++ app/_components/TimelineItem/DayOf.tsx
@@ -1,14 +1,7 @@
-import {
-  TimelineBody,
-  TimelineContent,
-  TimelineItem,
-  TimelineTime,
-  TimelinePoint,
-  TimelineTitle,
-} from "flowbite-react";
-import { List, ListItem } from "flowbite-react";
-import { LinkComponent } from "../Link";
-import { FigureComponent } from "../Figure";
+import { TimelineBody, TimelineContent, TimelineItem, TimelineTime, TimelinePoint, TimelineTitle } from 'flowbite-react';
+import { List, ListItem } from 'flowbite-react';
+import { LinkComponent } from '../Link';
+import { FigureComponent } from '../Figure';
 
 export const DayOf = () => (
   <TimelineItem>
@@ -19,21 +12,18 @@ export const DayOf = () => (
       <TimelineBody>
         <List ordered>
           <ListItem>
-            <strong>Arriving at Lake Travis Marina</strong>
+            <strong>
+              Arriving at <FigureComponent variant="p" imageSrc="/lake_travis_marina_pov.jpg" text="Lake Travis Marina" />
+            </strong>
           </ListItem>
           <List nested>
             <ListItem>
-              Use{" "}
+              Use{' '}
               <LinkComponent
                 href="https://www.google.com/maps/place/Lake+Travis+Marina/@30.4292748,-97.9236212,16z/data=!4m15!1m8!3m7!1s0x865b3056a61bb201:0x96d3067c8da37b92!2s6410+Hudson+Bend+Rd,+Austin,+TX+78734!3b1!8m2!3d30.4282892!4d-97.9245722!16s%2Fg%2F11cs7fjjqv!3m5!1s0x865b3057a8f8a21b:0xa9881a79ed6433d2!8m2!3d30.430536!4d-97.92144!16s%2Fg%2F1tf7h2f7?entry=ttu"
                 text="Google Maps"
-              />{" "}
-              or{" "}
-              <LinkComponent
-                href="http://maps.apple.com/?q=30.43065,-97.92159"
-                text="Apple Maps"
-              />{" "}
-              for directions.
+              />{' '}
+              or <LinkComponent href="http://maps.apple.com/?q=30.43065,-97.92159" text="Apple Maps" /> for directions.
             </ListItem>
           </List>
           <ListItem>
@@ -41,40 +31,21 @@ export const DayOf = () => (
           </ListItem>
           <List nested>
             <ListItem>
-              Inform the{" "}
-              <FigureComponent
-                variant="p"
-                imageSrc="/security_shack.png"
-                text="security guard"
-              />{" "}
-              that you’re heading to Slip C-46 under Cortez.
+              Inform the <FigureComponent variant="p" imageSrc="/security_shack.png" text="security guard" /> that you’re heading to Slip C-46 under Cortez.
             </ListItem>
             <ListItem>
-              Drive down the{" "}
-              <FigureComponent
-                imageSrc="/parking_lot.png"
-                text="hill"
-                variant="p"
-              />{" "}
-              and turn right into the dirt parking lot.
+              You can either park to the parking lot adjacent to the security shack or down hill past the security gate into the dirt{' '}
+              <FigureComponent imageSrc="/lake_travis_marina_parking.jpg" text="parking lot" variant="p" />.
             </ListItem>
-            <ListItem>
-              If the gate is closed, send us a message for the gate code.
-            </ListItem>
+            <ListItem>If the gate is closed, send us a message for the gate code.</ListItem>
           </List>
           <ListItem>
             <strong>Parking and Meeting</strong>
           </ListItem>
           <List nested>
             <ListItem>
-              {" "}
-              Walk down the{" "}
-              <FigureComponent
-                imageSrc="/ramp.png"
-                text="ramp"
-                variant="p"
-              />{" "}
-              to the marina with your belongings. We&apos;ll meet you at the
+              {' '}
+              Walk down the <FigureComponent imageSrc="/ramp.png" text="ramp" variant="p" /> to the marina with your belongings. We&apos;ll meet you at the
               gate.
             </ListItem>
           </List>
@@ -83,16 +54,11 @@ export const DayOf = () => (
           </ListItem>
           <List nested>
             <ListItem>
-              <FigureComponent
-                imageSrc="/google_maps_view.png"
-                text="Google Maps View"
-                variant="p"
-              />
+              <FigureComponent imageSrc="/google_maps_view.png" text="Google Maps View" variant="p" />
             </ListItem>
             <List nested>
               <ListItem>
-                <strong>Red</strong>: Drive into Lake Travis Marina via Hudson
-                Bend Rd.
+                <strong>Red</strong>: Drive into Lake Travis Marina via Hudson Bend Rd.
               </ListItem>
               <ListItem>
                 <strong>Purple</strong>: Security guard shack.
@@ -112,9 +78,7 @@ export const DayOf = () => (
             <strong>Before Boarding</strong>
           </ListItem>
           <List nested>
-            <ListItem>
-              Use the restroom at the marina, as there is no toilet on the boat.
-            </ListItem>
+            <ListItem>Use the restroom at the marina, as there is no toilet on the boat.</ListItem>
             <ListItem>Apply sunscreen before getting on the boat.</ListItem>
           </List>
         </List>
